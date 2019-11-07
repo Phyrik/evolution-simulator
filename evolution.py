9cd657da232797ded446731cf92813f3608f31c7
Phyrik <34688504+Phyrik@users.noreply.github.com>
2019-11-07 18:36:54 +0000

    Added eating_distance and other convenience tools

--- evolution.py
+++ evolution.py
@@ -1,4 +1,4 @@
-import random, math
+import random, math, time
 
 def distanceBetween(point1, point2):
     x_distance = abs(point1[0] - point2[0])
@@ -16,27 +16,29 @@ class Population:
     def newDay(self):
         self.added_individuals = 0
         self.minus_individuals = 0
+        self.killed_individuals = 0
         for individual in self.individuals:
             individual.nutrition -= 1
             if individual.nutrition < individual.survival_nutrition:
                 individual.kill()
+                return
             if individual.kill_nutrition <= individual.replication_nutrition:
                 if random.randrange(100) < individual.violence:
                     if individual.nutrition >= individual.kill_nutrition:
-                        individual.killIndividual()
+                        individual.killOtherIndividual()
                 else:    
                     if individual.nutrition >= individual.replication_nutrition:
-                        individual.replicate()
+                        individual.reproduce()
             elif individual.kill_nutrition > individual.replication_nutrition:
                 if individual.nutrition >= individual.kill_nutrition:
                     if random.randrange(100) < individual.violence:
-                        individual.killIndividual()
+                        individual.killOtherIndividual()
                     else:
                         if individual.nutrition >= individual.replication_nutrition:
-                            individual.replicate()
+                            individual.reproduce()
                 else:    
                     if individual.nutrition >= individual.replication_nutrition:
-                        individual.replicate()
+                        individual.reproduce()
             else:
                 pass
 
@@ -45,6 +47,7 @@ class Individual:
     def __init__(self, traits, population, position, min_nutritions):
         self.mutation_rate = traits['mutation_rate']
         self.vision_distance = traits['vision_distance']
+        self.eating_distance = traits['eating_distance']
         self.violence = traits['violence']
         self.population = population
         self.position = position
@@ -59,54 +62,73 @@ class Individual:
         self.population.individuals.remove(self)
         self.population.minus_individuals += 1
 
-    def replicate(self):
+    def reproduce(self):
         self.mutating = False
+
         self.mutation_rate_mutation_bool = random.randrange(100) < self.mutation_rate
         if self.mutation_rate_mutation_bool == True:
             self.mutation_rate_mutation = random.randrange(100)
             self.mutating = True
         else:
             self.mutation_rate_mutation = self.mutation_rate
+
         self.vision_distance_mutation_bool = random.randrange(100) < self.mutation_rate
         if self.vision_distance_mutation_bool == True:
-            self.vision_distance_mutation = random.randrange(100)
+            self.vision_distance_mutation = random.randrange(200)
             self.mutating = True
         else:
             self.vision_distance_mutation = self.vision_distance
+
+        self.eating_distance_mutation_bool = random.randrange(100) < self.mutation_rate
+        if self.eating_distance_mutation_bool == True:
+            self.eating_distance_mutation = random.randrange(125)
+            self.mutating = True
+        else:
+            self.eating_distance_mutation = self.eating_distance
+
         self.violence_mutation_bool = random.randrange(100) < self.mutation_rate
         if self.violence_mutation_bool == True:
             self.violence_mutation = random.randrange(100)
             self.mutating = True
         else:
             self.violence_mutation = self.violence
-        self.population.individuals.append(Individual({"mutation_rate": self.mutation_rate_mutation, "vision_distance": self.vision_distance_mutation, "violence": self.violence_mutation}, self.population, self.position, self.min_nutritions))
+
+        self.population.individuals.append(Individual({"mutation_rate": self.mutation_rate_mutation, "vision_distance": self.vision_distance_mutation, "eating_distance": self.eating_distance_mutation, "violence": self.violence_mutation}, self.population, [random.randint(1, 800), random.randint(1, 500)], self.min_nutritions))
         self.population.added_individuals += 1
         self.nutrition -= self.replication_nutrition
 
-    def killIndividual(self):
+    def killOtherIndividual(self):
         for individual in self.population.individuals:
             self.distance = distanceBetween(self.position, individual.position)
-            if self.distance <= self.vision_distance:
+            if self.distance <= self.eating_distance:
                 del self.population.individuals[self.population.individuals.index(individual)]
                 self.population.minus_individuals += 1
+                self.population.killed_individuals += 1
 
-    def determineNextPosition(self):
-        if self.food_to_be_eaten == None:
-            self.x = self.position[0]
-            self.y = self.position[1]
+    def determineNextPosition(self, list_of_food):
+        self.closest_food = Food((1000, 1000, 1000), 0)
+        self.list_of_food = list_of_food
+        self.foods_able_to_be_travelled_to = []
+        for food in self.list_of_food:
+            self.distance = distanceBetween(self.position, food.position)
+            if self.distance <= self.vision_distance:
+                self.foods_able_to_be_travelled_to.append(food)
+        if len(self.foods_able_to_be_travelled_to) != 0:
+            for food in self.foods_able_to_be_travelled_to:
+                if self.closest_food.position > food.position:
+                    self.closest_food = food
+            return self.closest_food.position
         else:
-            self.x = self.food_to_be_eaten.position[0]
-            self.y = self.food_to_be_eaten.position[1]
-        self.position = [self.x, self.y]
+            return self.position
 
         return (self.x, self.y)
 
     def eatFood(self, list_of_food):
         self.list_of_food = list_of_food
         self.foods_able_to_be_eaten = []
-        for food in list_of_food:
+        for food in self.list_of_food:
             self.distance = distanceBetween(self.position, food.position)
-            if self.distance <= self.vision_distance:
+            if self.distance <= self.eating_distance:
                 self.foods_able_to_be_eaten.append(food)
         if len(self.foods_able_to_be_eaten) != 0:
             for food in self.foods_able_to_be_eaten:
